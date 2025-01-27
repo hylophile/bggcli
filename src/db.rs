0c08a69ad16afd68578348b67198ba0f758870d0
hylo <hylo@posteo.de>
2025-01-27 22:29:03 +0100

    perf: build bulk insert queries before inserting

--- src/db.rs
+++ src/db.rs
@@ -1,143 +1,104 @@
 use anyhow::Result;
 use sqlx::{Pool, QueryBuilder, Sqlite};
-// use rusqlite::Connection;
-
-// pub async fn init(pool: &Pool<Sqlite>) -> Result<()> {
-//     sqlx::query!("",).execute(pool).await?;
-//     Ok(())
-// }
 
 pub async fn boardgames_insert(
     pool: &Pool<Sqlite>,
     boardgames: Vec<crate::boardgame::Item>,
 ) -> Result<()> {
     let mut item_query_builder: QueryBuilder<Sqlite> = QueryBuilder::new(
-        "
-INSERT OR REPLACE INTO item (
-    id,
-    item_type,
-    text,
-    thumbnail,
-    image,
-    description,
-    yearpublished,
-    minplayers,
-    maxplayers,
-    playingtime,
-    minplaytime,
-    maxplaytime,
-    minage
-) ",
+        "INSERT OR REPLACE INTO item (
+                   id,
+                   item_type,
+                   text,
+                   thumbnail,
+                   image,
+                   description,
+                   yearpublished,
+                   minplayers,
+                   maxplayers,
+                   playingtime,
+                   minplaytime,
+                   maxplaytime,
+                   minage
+               ) ",
     );
-    for b in boardgames {
-        dbg!(b.id);
+    let mut itemname_query_builder: QueryBuilder<Sqlite> = QueryBuilder::new(
+        "INSERT OR REPLACE INTO itemname (
+                   item_id,
+                   name_type,
+                   sortindex,
+                   value
+               ) VALUES ",
+    );
+    let mut link_query_builder: QueryBuilder<Sqlite> = QueryBuilder::new(
+        "INSERT OR REPLACE INTO link (
+                   id,
+                   item_id,
+                   link_type,
+                   value,
+                   inbound   
+               ) VALUES ",
+    );
 
-        //         sqlx::query!(
-        //             "
-        // INSERT OR REPLACE INTO item (
-        //     id,
-        //     item_type,
-        //     text,
-        //     thumbnail,
-        //     image,
-        //     description,
-        //     yearpublished,
-        //     minplayers,
-        //     maxplayers,
-        //     playingtime,
-        //     minplaytime,
-        //     maxplaytime,
-        //     minage
-        // )
-        //     VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-        //     ",
-        item_query_builder
-            .push_bind(b.id)
-            .push_bind(b.item_type)
-            .push_bind(b.text)
-            .push_bind(b.thumbnail)
-            .push_bind(b.image)
-            .push_bind(b.description)
-            .push_bind(b.yearpublished.value)
-            .push_bind(b.minplayers.value)
-            .push_bind(b.maxplayers.value)
-            .push_bind(b.playingtime.value)
-            .push_bind(b.minplaytime.value)
-            .push_bind(b.maxplaytime.value)
-            .push_bind(b.minage.value);
-        // )
-        // .execute(pool)
-        // .await?;
+    // TODO: feels very dumb, but it works
+    let mut is_first_link = true;
+    let mut is_first_itemname = true;
 
-        for l in b.link {
-            sqlx::query!(
-                "
-                INSERT OR REPLACE INTO link (
-                 id,
-                 item_id,
-                 link_type,
-                 value,
-                 inbound   
-                )
-                VALUES (?, ?, ?, ?, ?)
-            ",
-                l.id,
-                b.id,
-                l.link_type,
-                l.value,
-                l.inbound
-            )
-            .execute(pool)
-            .await?;
+    item_query_builder.push_values(boardgames, |mut item_qb, item| {
+        item_qb
+            .push_bind(item.id)
+            .push_bind(item.item_type)
+            .push_bind(item.text)
+            .push_bind(item.thumbnail)
+            .push_bind(item.image)
+            .push_bind(item.description)
+            .push_bind(item.yearpublished.value)
+            .push_bind(item.minplayers.value)
+            .push_bind(item.maxplayers.value)
+            .push_bind(item.playingtime.value)
+            .push_bind(item.minplaytime.value)
+            .push_bind(item.maxplaytime.value)
+            .push_bind(item.minage.value);
+
+        // TODO: manually building sql queries, in this day and age? well, it works
+        for link in item.link.iter() {
+            if !is_first_link {
+                link_query_builder.push(", ");
+            }
+            link_query_builder.push("(");
+            let mut separated = link_query_builder.separated(", ");
+            separated
+                .push_bind(link.id)
+                .push_bind(item.id)
+                .push_bind(link.link_type.clone())
+                .push_bind(link.value.clone())
+                .push_bind(link.inbound.clone());
+            separated.push_unseparated(")");
+            is_first_link = false;
         }
 
-        for n in b.name {
-            sqlx::query!(
-                "
-                    INSERT OR REPLACE INTO itemname (
-                        item_id,
-                        name_type,
-                        sortindex,
-                        value
-                    ) 
-                    VALUES (?, ?, ?, ?)
-                ",
-                b.id,
-                n.name_type,
-                n.sortindex,
-                n.value
-            )
-            .execute(pool)
-            .await?;
+        for name in item.name.iter() {
+            if !is_first_itemname {
+                itemname_query_builder.push(", ");
+            }
+            itemname_query_builder.push("(");
+            let mut separated = itemname_query_builder.separated(", ");
+            separated
+                .push_bind(item.id)
+                .push_bind(name.name_type.clone())
+                .push_bind(name.sortindex.clone())
+                .push_bind(name.value.clone());
+            separated.push_unseparated(")");
+            is_first_itemname = false;
         }
+    });
 
-        //     name: "first name".into(),
-        // };
-        // connection
-        //     .execute(
-        //         "INSERT INTO example (id, name) VALUES (:id, :name)",
-        //         to_params_named(&row1).unwrap().to_slice().as_slice(),
-        //     )
-        //     .unwrap();
-
-        // dbg!(b.primary_name());
-        // dbg!(b.mechanics());
-        // conn.execute(
-        //     "INSERT OR REPLACE INTO boardgames (id, name) VALUES (?1, ?2)",
-        //     (b.id, b.primary_name()),
-        // )?;
-        // for m in b.mechanics() {
-        //     conn.execute(
-        //         "INSERT OR REPLACE INTO mechanics (id, name) VALUES (?1, ?2)",
-        //         (m.id, m.name),
-        //     )?;
-        //     conn.execute(
-        //         "INSERT OR REPLACE INTO boardgames_mechanics (boardgame_id, mechanic_id) VALUES (?1, ?2)",
-        //         (b.id,m.id),
-        //     )?;
-        // }
-    }
-    item_query_builder.build().execute(pool).await?;
+    let q = item_query_builder.build();
+    q.execute(pool).await?;
+    let q = link_query_builder.build();
+    q.execute(pool).await?;
+    let q = itemname_query_builder.build();
+    q.execute(pool).await?;
 
     Ok(())
 }
